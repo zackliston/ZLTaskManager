a17f64c80af23edd48d229be1e74d4926bf4cc1b
Zack Liston <zackmliston@gmail.com>
2014-12-23 11:19:52 -0800

    Initial commit.

---
+++ ZLTaskManager/ZLTaskManager/ZLTaskManager.h
@@ -0,0 +1,19 @@
+//
+//  ZLTaskManager.h
+//  ZLTaskManager
+//
+//  Created by Zack Liston on 12/23/14.
+//  Copyright (c) 2014 Zack Liston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZLTaskManager.
+FOUNDATION_EXPORT double ZLTaskManagerVersionNumber;
+
+//! Project version string for ZLTaskManager.
+FOUNDATION_EXPORT const unsigned char ZLTaskManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZLTaskManager/PublicHeader.h>
+
+
